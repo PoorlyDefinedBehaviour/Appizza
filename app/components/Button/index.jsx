049175902f2903b0c8095397b54989ce79d711fb
João Victor <joaovictorvisona@gmail.com>
2020-03-07 18:34:21 -0300

    App project setup

---
+++ app/components/Button/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ButtonContainer, ButtonText } from './styled';
+
+const CustomButton = ({ backgroundColor, textColor, text, onPress }) => (
+	<ButtonContainer
+		onPress={onPress}
+		backgroundColor={backgroundColor}
+	>
+		<ButtonText textColor={textColor}>{text}</ButtonText>
+	</ButtonContainer>
+);
+
+export default CustomButton;
---
+++ app/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '../components/Button';
+import Page from "../components/Page";
+
+const Login = () => {
+	return (
+		<Page>
+			<Button backgroundColor="#ff0000" text="Ola mundo" textColor="#fff" onPress={() => alert('Funcionou')}>
+				Botão
+			</Button>
+		</Page>
+	);
+};
+
+export default Login;
