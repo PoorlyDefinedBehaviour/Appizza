f0c2cbd1792e38ec2d427855fce1a65d3823667f
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-03-13 21:06:04 -0300

    feat: create pagination object type

---
+++ server/src/Contracts/Pagination.ts
@@ -0,0 +1,30 @@
+import { Field, ObjectType, Int } from "type-graphql"
+
+interface PaginationData {
+  skip: number
+  take: number
+  count: number
+  has_next: boolean
+}
+
+@ObjectType("pagination")
+export default class Pagination {
+  constructor(data: PaginationData) {
+    this.skip = data.skip
+    this.take = data.take
+    this.count = data.count
+    this.has_next = data.has_next
+  }
+
+  @Field(() => Boolean)
+  has_next: boolean
+
+  @Field(() => Int)
+  skip: number
+
+  @Field(() => Int)
+  take: number
+
+  @Field(() => Int)
+  count: number
+}
